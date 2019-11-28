cc45fe2f8fcd142c9a9e88995dd0fa3cf104ee45
RGates94 <rgates275@gmail.com>
2019-11-28 10:10:06 -0500

    Add update_column method to Puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use array_init::array_init;
 use crate::Cell::{Given, Guesses};
+use array_init::array_init;
 
 const BOX_SIZE: usize = 3;
 const GRID_SIZE: usize = BOX_SIZE * BOX_SIZE;
@@ -13,18 +13,18 @@ struct SolvedCell {
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 enum Cell {
     Given(SolvedCell),
-    Guesses([bool;GRID_SIZE])
+    Guesses([bool; GRID_SIZE]),
 }
 
 #[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
 struct Puzzle {
-    cells: [[Cell;GRID_SIZE];GRID_SIZE],
+    cells: [[Cell; GRID_SIZE]; GRID_SIZE],
 }
 
 impl Puzzle {
     fn update_row(&mut self, index: usize) {
         if let Some(row) = self.cells.get_mut(index) {
-            let mut cell_mask = [true;GRID_SIZE];
+            let mut cell_mask = [true; GRID_SIZE];
             for cell in row.iter() {
                 if let Given(cell) = cell {
                     if let Some(guess) = cell_mask.get_mut(cell.value as usize) {
@@ -34,7 +34,33 @@ impl Puzzle {
             }
             for cell in row.iter_mut() {
                 if let Guesses(guesses) = cell {
-                    guesses.iter_mut().zip(cell_mask.iter_mut()).for_each(|(guess, mask)| *guess &= *mask)
+                    guesses
+                        .iter_mut()
+                        .zip(cell_mask.iter_mut())
+                        .for_each(|(guess, mask)| *guess &= *mask)
+                }
+            }
+        }
+    }
+    fn update_column(&mut self, index: usize) {
+        if index < GRID_SIZE {
+            let mut cell_mask = [true; GRID_SIZE];
+            for row in self.cells.iter_mut() {
+                let cell = row
+                    .get_mut(index)
+                    .expect("Column index out of bounds unexpectedly");
+                if let Given(cell) = cell {
+                    if let Some(guess) = cell_mask.get_mut(cell.value as usize) {
+                        *guess = false;
+                    }
+                }
+            }
+            for row in self.cells.iter_mut() {
+                if let Some(Guesses(guesses)) = row.get_mut(index) {
+                    guesses
+                        .iter_mut()
+                        .zip(cell_mask.iter_mut())
+                        .for_each(|(guess, mask)| *guess &= *mask)
                 }
             }
         }
@@ -45,26 +71,59 @@ impl From<String> for Puzzle {
     fn from(input: String) -> Self {
         let chars = input.chars().collect::<Vec<_>>();
         Self {
-            cells: array_init(|row| array_init::<[Cell;GRID_SIZE], _>(|column| match chars.get(9 * row + column) {
-                Some('1') => Given(SolvedCell {value: 0, given: true }),
-                Some('2') => Given(SolvedCell {value: 1, given: true }),
-                Some('3') => Given(SolvedCell {value: 2, given: true }),
-                Some('4') => Given(SolvedCell {value: 3, given: true }),
-                Some('5') => Given(SolvedCell {value: 4, given: true }),
-                Some('6') => Given(SolvedCell {value: 5, given: true }),
-                Some('7') => Given(SolvedCell {value: 6, given: true }),
-                Some('8') => Given(SolvedCell {value: 7, given: true }),
-                Some('9') => Given(SolvedCell {value: 8, given: true }),
-                _ => Guesses([true;GRID_SIZE]),
-            }
-            )),
+            cells: array_init(|row| {
+                array_init::<[Cell; GRID_SIZE], _>(|column| match chars.get(9 * row + column) {
+                    Some('1') => Given(SolvedCell {
+                        value: 0,
+                        given: true,
+                    }),
+                    Some('2') => Given(SolvedCell {
+                        value: 1,
+                        given: true,
+                    }),
+                    Some('3') => Given(SolvedCell {
+                        value: 2,
+                        given: true,
+                    }),
+                    Some('4') => Given(SolvedCell {
+                        value: 3,
+                        given: true,
+                    }),
+                    Some('5') => Given(SolvedCell {
+                        value: 4,
+                        given: true,
+                    }),
+                    Some('6') => Given(SolvedCell {
+                        value: 5,
+                        given: true,
+                    }),
+                    Some('7') => Given(SolvedCell {
+                        value: 6,
+                        given: true,
+                    }),
+                    Some('8') => Given(SolvedCell {
+                        value: 7,
+                        given: true,
+                    }),
+                    Some('9') => Given(SolvedCell {
+                        value: 8,
+                        given: true,
+                    }),
+                    _ => Guesses([true; GRID_SIZE]),
+                })
+            }),
         }
     }
 }
 
 fn main() {
-    let mut puzzle: Puzzle = String::from("100009006020700050003080400009400003080050100700006020000003700000020080000100009").into();
+    let mut puzzle: Puzzle = String::from(
+        "100009006020700050003080400009400003080050100700006020000003700000020080000100009",
+    )
+    .into();
     println!("{:?}", puzzle);
     puzzle.update_row(0);
+    println!("{:?}", puzzle);
+    puzzle.update_column(0);
     println!("{:?}", puzzle);
 }
